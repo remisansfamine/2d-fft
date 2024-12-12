2176c2410590d10bfeba7749c8c56a8480535efb
Guillaume Chiclet <g.chiclet@student.isartdigital.com>
2024-12-12 10:41:46 +0100

    add project with fft function
    Co-authored-by: Rémi GINER <remi.giner@hotmail.fr>
    Co-authored-by: Lancelot MARÉCHAL <l.marechal@student.isartdigital.com>

---
+++ Assets/Scripts/FFT.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class FFT
+{
+    static public Complex[] Forward(Complex[] input)
+    {
+        int n = input.Length;
+
+        if (n == 1)
+            return input;
+
+        int halfN = n / 2;
+
+        Complex omegaN = Complex.Exp(-2f * Mathf.PI * Complex.ImaginaryOne / n);
+
+        Complex[] evenInput = new Complex[halfN];
+        Complex[] oddInput = new Complex[halfN];
+
+        for (int i = 0; i < halfN; i++)
+        {
+            evenInput[i] = input[i * 2];
+            oddInput[i] = input[i * 2 + 1];
+        }
+
+        Complex[] even = Forward(evenInput);
+        Complex[] odd = Forward(oddInput);
+
+
+        Complex[] result = new Complex[n];
+        Complex omega = 1;
+
+        for (int k = 0; k < halfN; k++)
+        {
+            result[k] = even[k] + omega * odd[k];
+            result[k + halfN] = even[k] - omega * odd[k];
+            omega *= omegaN;
+        }
+
+        return result;
+    }
+}
---
+++ Assets/Scripts/SignalEmitter.cs
@@ -0,0 +1,49 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SignalEmitter : MonoBehaviour
+{
+    private void Start()
+    {
+        FourierTest();
+    }
+
+    public void FourierTest()
+    {
+        Complex[] signal = new[]
+        {
+            new Complex(0,0),
+            new Complex(1,0),
+            new Complex(2,0),
+            new Complex(3,0),
+            new Complex(4,0),
+            new Complex(5,0),
+            new Complex(6,0),
+            new Complex(7,0)
+        };
+
+        Complex[] result = FFT.Forward(signal);
+
+        Complex[] expected = new[]
+        {
+            new Complex(28, 0),
+            new Complex(-4.0f, 9.6569f),
+            new Complex(-4.0f, 4.0f),
+            new Complex(-4.0f, 1.6569f),
+            new Complex(-4, 0.0f),
+            new Complex(-4, -1.6569f),
+            new Complex(-4, -4.0000f),
+            new Complex(-4, -9.6569f)
+        };
+
+        for (int i = 0; i < expected.Length; i++)
+        {
+            Assert.AreEqual(System.Math.Round(expected[i].Real, 4), System.Math.Round(result[i].Real, 4));
+            Assert.AreEqual(System.Math.Round(expected[i].Imaginary, 4), System.Math.Round(result[i].Imaginary, 4));
+        }
+    }
+}
